9c0fdaa9bce37cacbcaf58f4b9b57f7daacb8c93
Dinesh Kumar Choudhary <mapmydestination@gmail.com>
2021-02-12 19:51:32 +0530

    update

--- Admin/logout.php
+++ Admin/logout.php
@@ -1,18 +1,12 @@
-<?php session_start();?>
-<!DOCTYPE html>
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title></title>
-</head>
-
-<body>
-<?php
-$_SESSION["username"]="";
-$_SESSION["usertype"]="";
-$_SESSION["loginstatus"]="";
-
-header("location:loginform.php");
+<?php  
+session_start(); //to ensure you are using same session
+session_destroy(); //destroy the session
+ 
 ?>
-</body>
-</html>
+<script>
+window.location="page-login.php";
+</script>
+<?php
+//to redirect back to "index.php" after logging out
+  exit;
+?>
